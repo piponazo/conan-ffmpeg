d67b58db48791d4c783d72a37a2ef33431bc8bf6
Luis Díaz Más <piponazo@gmail.com>
2017-07-25 21:35:26 +0200

    First try to cross-compilation

--- conanfile.py
+++ conanfile.py
@@ -22,14 +22,13 @@ class FFmpegConan(ConanFile):
         CXX = os.getenv('CXX')
 
         with tools.environment_append(env.vars):
-            self.run("cd %s && ./configure \
+            configure_command = "./configure \
                      --enable-pic \
                      --enable-shared \
                      --disable-static \
                      --disable-symver \
                      --disable-ffplay \
                      --disable-ffprobe \
-                     --enable-pthreads \
                      --disable-devices \
                      --disable-avdevice \
                      --disable-nonfree \
@@ -43,15 +42,29 @@ class FFmpegConan(ConanFile):
                      --disable-swresample \
                      --cc=%s \
                      --cxx=%s \
-                     --prefix=%s" % (self.name, CC, CXX, self.package_folder))
+                     --prefix=%s" % (CC, CXX, self.package_folder)
 
+            if self.settings.os == "Windows":
+                configure_command += " --arch=x86 --target-os=mingw32 --enable-cross-compile"
+            else:
+                configure_command += " --enable-pthreads"
+
+            self.run("cd %s && %s" % (self.name, configure_command))
             self.run("cd %s && make -j%s" % (self.name, n_cores))
-            self.run("cd %s && make install" % self.name)
+
+            if not tools.os_info.is_windows:
+                self.run("cd %s && make install" % self.name)
 
     def package(self):
-        self.copy('FindFFmpeg.cmake', '.', '.')
-        self.copy("*.h",  dst="include", src="installFolder/include")
-        self.copy("*.so", dst="lib",     src="installFolder/lib")
+        if tools.os_info.is_windows:
+            self.copy('FindFFmpeg.cmake', '.', '.')
+            self.copy(pattern="*.h", dst="include/libavcodec", src="%s/libavcodec" % self.name)
+            self.copy(pattern="*.a", dst="lib/", src="%s/libavcodec" % self.name)
+            self.copy(pattern="*.dll", dst="bin/", src="%s/libavcodec" % self.name)
+        else:
+            self.copy('FindFFmpeg.cmake', '.', '.')
+            self.copy("*.h",  dst="include", src="installFolder/include")
+            self.copy("*.so", dst="lib",     src="installFolder/lib")
 
     def package_info(self):
         self.cpp_info.includedirs = ["include"]  # Ordered list of include paths
